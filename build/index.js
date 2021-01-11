6d52f77cf5947bbc5863bd83746746526acdd1b7
Sergey <siniiitsa@gmail.com>
2021-01-11 19:18:49 +0200

    implement Sorter for numbers

--- build/index.js
+++ build/index.js
@@ -1,6 +1,23 @@
 "use strict";
-var logSomething = function (msg) {
-    if (msg === void 0) { msg = 'something'; }
-    console.log(msg);
-};
-logSomething();
+var Sorter = /** @class */ (function () {
+    function Sorter(collection) {
+        this.collection = collection;
+    }
+    Sorter.prototype.sort = function () {
+        var length = this.collection.length;
+        for (var i = 0; i < length; i++) {
+            for (var j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    var temp = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = temp;
+                }
+            }
+        }
+    };
+    return Sorter;
+}());
+var numbers = [10, -3, 5, 0];
+var sorter = new Sorter(numbers);
+sorter.sort();
+console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
-const logSomething = (msg = 'something') => {
-  console.log(msg);
-};
+class Sorter {
+  constructor(public collection: number[]) {}
 
-logSomething();
+  sort(): void {
+    const { length } = this.collection;
+
+    for (let i = 0; i < length; i++) {
+      for (let j = 0; j < length - i - 1; j++) {
+        if (this.collection[j] > this.collection[j + 1]) {
+          const temp = this.collection[j];
+          this.collection[j] = this.collection[j + 1];
+          this.collection[j + 1] = temp;
+        }
+      }
+    }
+  }
+}
+
+const numbers = [10, -3, 5, 0];
+const sorter = new Sorter(numbers);
+sorter.sort();
+console.log(sorter.collection);
